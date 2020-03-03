3da446ee5867bbda0892f0ba3a4f84f68ee9d95b
Muhun Kim <iam@muhun.kim>
2020-03-03 13:25:12 +0900

    Update ItemStore

--- src/store/ItemStore.js
+++ src/store/ItemStore.js
@@ -1,4 +1,41 @@
-import { observable, computed, action } from 'mobx';
-import Item from './ItemModel';
+import { observable, computed, action } from "mobx";
+import Item from "./ItemModel";
 
-export default class ItemStore {}
+export default class ItemStore {
+  @observable items = [];
+  @observable unpackedItemsFilter = "";
+  @observable packedItemsFilter = "";
+
+  @computed get packedItems() {
+    return this.items.filter(item => item.packed);
+  }
+
+  @computed get unpackedItems() {
+    return this.items.filter(item => item.unpacked);
+  }
+
+  @computed get filteredPackedList() {
+    return this.packedItems.filter(item =>
+      item.value.includes(this.packedItemsFilter)
+    );
+  }
+
+  @computed get filteredUnPackedList() {
+    return this.unpackedItems.filter(item =>
+      item.value.includes(this.unpackedItemsFilter)
+    );
+  }
+
+  @action.bound
+  addItems(items) {}
+
+  @action.bound
+  updatePackedItemsFilter(value) {
+    this.packedItemsFilter = value;
+  }
+
+  @action.bound
+  updateUnpackedItemsFilter(value) {
+    this.unpackedItemsFilter = value;
+  }
+}
