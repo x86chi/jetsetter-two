b0a820356aa4b602715f4d9a24f6139d9acdd207
Muhun Kim <iam@muhun.kim>
2020-03-03 19:50:45 +0900

    Sending Data from React into Mobx

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,13 @@ import { inject, observer } from "mobx-react";
 import NewItem from "./NewItem";
 import Items from "./Items";
 
-const UnpackedItems = inject("itemsList")(
+const UnpackedItems = inject("itemList")(
   observer(({ itemList }) => (
     <Items title="Unpacked Items" items={itemList.unpackedItems} />
   ))
 );
 
-const PackedItems = inject("itemsList")(
+const PackedItems = inject("itemList")(
   observer(({ itemList }) => (
     <Items title="Unpacked Items" items={itemList.packedItems} />
   ))
